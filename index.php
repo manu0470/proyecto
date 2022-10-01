29fa16aa756e270dcdb0d66c5406fcf66f1da6b4
manu0470 <102345573+manu0470@users.noreply.github.com>
2022-09-30 21:41:12 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+       <title>CURRICULUM VITAE</title>
+           <script src="https://kit.fontawesome.com/a85adf3762.js" crossorigin="anonymous"></script>
+
+
+
+    </head>
+
+    <body>
+
+      <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo"><b>CURRICULUM VITAE</b></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">SOBRE MI</a></li>
+        <li><a href="badges.html">ESTUDIOS</a></li>
+        <li><a href="collapsible.html">PORTAFOLIO</a></li>
+        <li><a href="badges.html">INTERESES</a></li>
+        <li><a href="collapsible.html">CONTACTO</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <section id="yo" style="background: rgba(0, 0, 0, 0.1);">
+
+  <table border="5px" align="center">
+    
+    <tr>
+      <td width="40%"><div align="center">
+        <p style="font-size:30px ;">CARLOS MANUEL ZAVALA SALAS</p>
+        <br>
+        <img src="img/fotoM.jpg" alt="" width="70%" height="70%">
+        <br>
+        <p style="font-size:30px ;">CURRICULUM VITAE</p>
+      </div>
+      </td>
+
+      <td><p style="font-size:30px ; text-align: justify;">TRABAJA MIENTRAS OTROS DUERMAN, ESTUDIA MIENTRAS OTROS SE DIVIERTEN, PERSISTE MIENTRAS OTROS DESCANSAN Y LUEGO VIVIRÁS LO QUE HAS SOÑADO!</p></td>
+    </tr>
+
+
+  </table>
+
+</section>
+<section id="estudios">
+  <p style="font-size: 70px; text-align: center;">ESTUDIOS</p>
+  <table align="center" width="50%">
+    
+    <tr>
+      <td width="10%">
+        <div align="center">
+        <ul class="collection">
+      <li class="collection-item"><img src="img/imtc.jpg"><br>PRIMARIA: INSTITUTO MARÍA TERESA DE CANCINO</li>
+      <li class="collection-item"><img src="img/moises.jpg"><br>SECUNDARIA: PROFESOR MOISES SAENZ</li>
+      <li class="collection-item"><img src="img/cetis.jpg"><br>PREPARATORIA: CENTRO DE ESTUDIOS TECNOLÓGICOS industrial y de servicio No. 67</li>
+      <li class="collection-item"><img src="img/uth.jpg" width="50%" height="50%"><br>UNIVERSIDAD: UNIVERSIDAD TECNOLÓGICA DE HUEJOTZINGO</li>
+    </ul>
+      </div>
+      </td>
+    </tr>
+
+
+  </table>
+</section>
+
+
+<section id="portafolio" style="background: rgba(0, 0, 0, 0.1);">
+
+  <p style="font-size: 70px; text-align: center;">PORTAFOLIO</p>
+
+  <div class="row">
+    <div class="col s6">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/web.jpg">
+          <span class="card-title"><p style="color: white;">DESARROLLO WEB</p></span>
+        </div>
+        <div class="card-content">
+          <p>ESPECIALIDAD EN DESARROLLO FRONT END</p>
+          <br>
+          <p>TECNOLOGÍAS MANEJADAS</p>
+          <br>
+          <ul>
+            <li>JAVASCRIPT</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>VUE.JS</li>
+            <li>BOOTSTRAP</li>
+            <li>ANGULAR</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s6">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/hacker.jpg">
+          <span class="card-title"><p style="color: white;">HACKING ÉTICO</p></span>
+        </div>
+        <div class="card-content">
+          <p>ESPECIALIDAD EN SEGURIDAD INFORMÁTICA</p>
+          <br>
+          <p>RAMAS MANEJADAS</p>
+          <br>
+          <ul>
+            <li>KALI LINUX</li>
+            <li>ARCH LINUX</li>
+            <li>CRACKER</li>
+            <li>COPYHACKER</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    
+</section>
+
+
+<section id="intereses">
+<p style="font-size: 70px; text-align: center;">INTERESES</p>
+
+  <div class="row">
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/lectura.jpg">
+          <span class="card-title"><p style="color: black;">LECTURA</p></span>
+        </div>
+        <div class="card-content">
+          <p>Libros Leídos: Harry Potter, El código da Vinci, El gran libro de HTML, CSS y JAVASCRIPT</p>
+        </div>
+      </div>
+    </div>
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/deporte.jpg">
+          <span class="card-title"><p style="color: black;">DEPORTES</p></span>
+        </div>
+        <div class="card-content">
+          <ul>
+            <li>FUTBOL</li>
+            <li>BASEBALL</li>
+            <li>TENNIS</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col s4">
+      <div class="card">
+        <div class="card-image">
+          <img src="img/musica.jpg">
+          <span class="card-title"><p style="color: black;">MÚSICA</p></span>
+        </div>
+        <div class="card-content">
+          <ul>
+            <li>RANCHERA</li>
+            <li>CUMBIAS</li>
+            <li>CUMBIAS SONIDERAS</li>
+            <li>BALADAS</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section id="contacto" style="background: rgba(0, 0, 0, 0.1);">
+  <p style="font-size: 70px; text-align: center;">CONTACTO</p>
+
+  <div class="row" align="center">
+    <div class="col s6">
+      <h1>REDES SOCIALES</h1>
+      <br>
+      <i class="fa-brands fa-facebook" style="color:blue;"></i>
+                    <i class="fa-brands fa-telegram" style="color:#00acee;"></i>
+                    <i class="fa-brands fa-twitter" style="color:#00acee;"></i>
+    </div>
+
+    <div class="col s6">
+      <i class="fa-solid fa-location-dot" style="color:red;"></i><h5>MÉXICO</h5>
+                    <i class="fa-solid fa-mobile-screen"></i><h5>+52 222 111 2297</h5>
+                    <i class="fa-regular fa-envelope"></i><h5>manu@gmail.com</h5>
+    </div>
+  </div>
+
+</section>
+<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">FRASE</h5>
+                <p class="grey-text text-lighten-4" style="text-align: justify;">TRABAJA MIENTRAS OTROS DUERMAN, ESTUDIA MIENTRAS OTROS SE DIVIERTEN, PERSISTE MIENTRAS OTROS DESCANSAN Y LUEGO VIVIRÁS LO QUE HAS SOÑADO!</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">PROFESIONES</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">HACKER DE SOMBRERO BLANCO</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">DESARROLLADOR FRONTEND</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">TÉCNICO</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2022 DERECHOS RESERVADOS
+            <a class="grey-text text-lighten-4 right" href="#!">SITIO WEB</a>
+            </div>
+          </div>
+        </footer>
+
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
